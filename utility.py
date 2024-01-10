3026f8083b61ff9cdf7d866d473bfb1a255dd077
zaqks <imzakbro@gmail.com>
2024-01-10 15:16:34 +0100

    utility support

---
+++ utility.py
@@ -0,0 +1,39 @@
+from utilityTasks.tasks import *
+
+
+class Vars:
+    run = True
+
+
+def toggleRun():
+    Vars.run = not Vars.run
+
+
+funcs = [toggleRun, compileS, build, merge]
+choices = [f"toggle run: {Vars.run}", "compile", "build", "merge"]
+choice = -1
+
+while choice > funcs.__len__() - 1 or choice < 1:
+    # display menu
+    print("welcome to C project utility tools")
+    print("please select an operation")
+
+    for i in range(choices.__len__()):
+        print(f"{i}- {choices[i]}")
+    print("\n")
+
+    # user input
+    try:
+        choice = int(input())
+    except:
+        choice = -1
+
+    # for run t0ggle
+    if choice == 0:
+        funcs[choice]()
+        choices[0] = f"toggle run: {Vars.run}"
+
+
+funcs[choice]()
+if Vars.run:
+    system("./bin/main")
